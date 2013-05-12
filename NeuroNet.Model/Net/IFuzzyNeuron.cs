7149d1ff7ec8eb1d4e46f90554806d85ed7bfdee
Diover <istatishere@gmail.com>
2013-05-12 15:21:40 -0700

    Cosmetics. Some changes for better readability.

--- NeuroNet.Model/Net/IFuzzyNeuron.cs
+++ NeuroNet.Model/Net/IFuzzyNeuron.cs
@@ -6,11 +6,13 @@ namespace NeuroNet.Model.Net
     public interface IFuzzyNeuron
     {
         void AddInput(ILink link);
+        void AddInput(ILink link, IFuzzyNumber weight);
         void SetOutput(ILink link);
         void Propagate();
-        void AddInput(ILink link, IFuzzyNumber weight);
+
         IFuzzyNumber LastOutput { get; }
         IFuzzyNumber PropagatedError { get; set; }
+
         ILink GetWeight(int i);
         ILink GetWeightDelta(int i);
         void ForeachWeight(Action<int, ILink> action);
