bc5894d7b2c4674553c0a2cb24f3f63ec4ca86d7
Diover <istatishere@gmail.com>
2013-05-25 05:40:02 -0700

    ConsoleApp. Added CreatePatterns() function.

--- NeuroNet.ConsoleApp/Program.cs
+++ NeuroNet.ConsoleApp/Program.cs
@@ -13,9 +13,30 @@ namespace NeuroNet.ConsoleApp
 {
     class Program
     {
+        static List<ILearningPattern> CreatePatterns(Func<double, double, double> f)
+        {
+            var patternsCount = 5;
+            var step = 0.5;
+
+            var a = -100.0;
+            var b = 100.0;
+
+            var result = new List<ILearningPattern>();
+            for (double i = a; i < a + step * patternsCount; i += step)
+                for (double j = b; j < b + step*patternsCount; j += step)
+                {
+                    if(f(i, j) > 1.0 || f(i, j) < 0.0)
+                        throw new ArgumentException("f(" + i + ", " + j + ") = " + f(i, j) + " is out of [0,1]");
+                    
+                    result.Add(new LearningPattern(new List<IFuzzyNumber> {new RealNumber(i), new RealNumber(j)},
+                                                   new List<IFuzzyNumber> {new RealNumber(f(i, j))}));
+                }
+            return result;
+        }
+
         static void Main(string[] args)
         {
-            var b = new BfgsMethod();
+            /*var b = new BfgsMethod();
             var f =
                 new Func<IVector, double>(
                     vector =>
@@ -35,6 +56,7 @@ namespace NeuroNet.ConsoleApp
 
             var x = b.Values;
             var fx = b.Minimum;
+            */
 
             const int inputsCount = 2;
             const int hiddenNeuronsCount = 2;
@@ -43,13 +65,14 @@ namespace NeuroNet.ConsoleApp
             
             //var patterns = new TestPatternPreparer("testPatterns.txt", new FuzzyNumberParser()).PreparePatterns();
             var patterns = new TestPatternPreparer("testPatternsReal.txt", new RealNumberParser()).PreparePatterns();
+            //var patterns = CreatePatterns((x, y) => Math.Abs(Math.Sin(x) + Math.Sin(y))/2.0);
 
             var bp = new BackPropagation(patterns);
             bp.CyclePerformed +=
                 (state) =>
                 {
-                    //Console.ReadKey();
-                    if (state.Cycle % 100 == 0)
+                    Console.ReadKey();
+                    //if (state.Cycle % 100 == 0)
                         Console.WriteLine("cycle: " + state.Cycle +
                                           " error: " + state.CycleError.ToString("0.#####################"));
                 };
