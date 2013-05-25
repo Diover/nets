99113f7a14e42367e8a2eb21ed859e7c30e30afd
Diover <istatishere@gmail.com>
2013-05-25 10:48:02 -0700

    Tests. Added some tests.

--- NeuroNet.Model.Tests/Net/SimpleFuzzyNetShould.cs
+++ NeuroNet.Model.Tests/Net/SimpleFuzzyNetShould.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using NeuroNet.Model.FuzzyNumbers;
+using NeuroNet.Model.FuzzyNumbers.Vectors;
 using NeuroNet.Model.Net;
 
 namespace NeuroNet.Model.Tests.Net
@@ -245,7 +246,6 @@ namespace NeuroNet.Model.Tests.Net
             }
         }
 
-
         [Test]
         public void PropagateSignalWithZeroWeights()
         {
@@ -276,6 +276,210 @@ namespace NeuroNet.Model.Tests.Net
             }
         }
 
+        [Test]
+        public void SetPropagatedErrorToNullAfterClear()
+        {
+            const int inputs = 2;
+            var hidden = new[] { 2, 2 };
+            var net = new SimpleFuzzyNet(inputs, hidden, () => new RealNumber(0.0));
+
+            SetPropagatedErrorToZero(net);
+            net.ClearPropagatedError();
+
+            foreach (var layer in net.Layers)
+            {
+                layer.ForeachNeuron((i, neuron) => Assert.IsNull(neuron.PropagatedError));
+            }
+        }
+
+        private static void SetPropagatedErrorToZero(INet net)
+        {
+            foreach (var layer in net.Layers)
+            {
+                layer.ForeachNeuron((i, neuron) =>
+                {
+                    neuron.PropagatedError = new RealNumber(0.0);
+                });
+            }
+        }
+
+        [Test]
+        public void CorrectlyCreateWeightsVector()
+        {
+            const int inputs = 2;
+            var hidden = new[] { 2, 2 };
+            const int outputs = 2;
+            var net = new SimpleFuzzyNet(inputs, hidden, GenerateNumbers, outputNeuronsCount: outputs);
+
+            var weights = net.GetWeights();
+            var expectedWeights = new Vector(new IFuzzyNumber[]
+                {
+                    //output layer
+                    //first neuron
+                    new RealNumber(2.0),
+                    new RealNumber(2.0),
+                    //second neuron
+                    new RealNumber(4.0),
+                    new RealNumber(2.0),
+
+                    //hidden 1
+                    new RealNumber(1.0),
+                    new RealNumber(-1.5),
+                    new RealNumber(2.0),
+                    new RealNumber(1.0),
+
+                    //hidden 2
+                    new RealNumber(-1.0),
+                    new RealNumber(-1.0),
+                    new RealNumber(2.0),
+                    new RealNumber(-1.0),
+                });
+
+            for (int i = 0; i < expectedWeights.Length; i++)
+            {
+                Assert.That(weights[i].GetMod().X, Is.EqualTo(expectedWeights[i].GetMod().X));
+            }
+        }
+
+        [Test]
+        public void CorrectlySetWeightsVector()
+        {
+            const int inputs = 2;
+            var hidden = new[] { 2, 2 };
+            const int outputs = 2;
+            var net = new SimpleFuzzyNet(inputs, hidden, ()=>new RealNumber(0.0), outputNeuronsCount: outputs);
+            var weights = new Vector(new IFuzzyNumber[]
+                {
+                    //output layer
+                    //first neuron
+                    new RealNumber(2.0),
+                    new RealNumber(2.0),
+                    //second neuron
+                    new RealNumber(4.0),
+                    new RealNumber(2.0),
+
+                    //hidden 1
+                    new RealNumber(1.0),
+                    new RealNumber(-1.5),
+                    new RealNumber(2.0),
+                    new RealNumber(1.0),
+
+                    //hidden 2
+                    new RealNumber(-1.0),
+                    new RealNumber(-1.0),
+                    new RealNumber(2.0),
+                    new RealNumber(-1.0),
+                });
+
+            //tuple: (layer, neuron, weight)
+            var expectedWeights = new Dictionary<Tuple<int, int, int>, IFuzzyNumber>();
+            for (int i = 0; i < weights.Length; i++)
+            {
+                expectedWeights.Add(new Tuple<int, int, int>(ToLayerIndex(i), ToNeuronIndex(i), ToWeightIndex(i)),
+                                    weights[i]);
+            }
+            
+            net.SetWeights(weights);
+
+            int l = 0;
+            foreach (var layer in net.Layers)
+            {
+                layer.ForeachNeuron((i, neuron) =>
+                {
+                    neuron.ForeachWeight((j, weight) =>
+                        {
+                            Assert.That(weight.Signal.GetMod().X,
+                                        Is.EqualTo(expectedWeights[new Tuple<int, int, int>(l, i, j)].GetMod().X));
+                        });
+                });
+                l++;
+            }
+        }
+
+        private static int ToLayerIndex(int i)
+        {
+            if (i >= 0 && i < 4)
+                return 2;
+            if (i >= 4 && i < 8)
+                return 0;
+            if (i >= 8 && i < 12)
+                return 1;
+            return -1;
+        }
+
+        private static int ToWeightIndex(int i)
+        {
+            return i % 2;
+        }
+
+        private static int ToNeuronIndex(int i)
+        {
+            switch (i % 4)
+            {
+                case 0:
+                case 1:
+                    return 0;
+                case 2:
+                case 3:
+                    return 1;
+            }
+            return -1;
+        }
+
+        [Test]
+        public void PropagateSignalAfterWeightsChanged()
+        {
+            const int inputs = 2;
+            var hidden = new[] { 2, 2 };
+            const int outputs = 2;
+            Func<double, double> activation = x => x;
+            var net = new SimpleFuzzyNet(inputs, hidden, () => new RealNumber(0.0), activation, outputs);
+
+            var input = new List<IFuzzyNumber>
+                {
+                    new RealNumber(1.0),
+                    new RealNumber(2.0),
+                };
+            var weights = new Vector(new IFuzzyNumber[]
+                {
+                    //output layer
+                    //first neuron
+                    new RealNumber(2.0),
+                    new RealNumber(2.0),
+                    //second neuron
+                    new RealNumber(4.0),
+                    new RealNumber(2.0),
+
+                    //hidden 1
+                    new RealNumber(1.0),
+                    new RealNumber(-1.5),
+                    new RealNumber(2.0),
+                    new RealNumber(1.0),
+
+                    //hidden 2
+                    new RealNumber(-1.0),
+                    new RealNumber(-1.0),
+                    new RealNumber(2.0),
+                    new RealNumber(-1.0),
+                });
+
+            net.SetWeights(weights);
+            var output = net.Propagate(input);
+
+            var expected = new List<IFuzzyNumber>
+                {
+                    new RealNumber(-20.0),
+                    new RealNumber(-24.0),
+                };
+
+            int i = 0;
+            foreach (var expectedNumber in expected)
+            {
+                Assert.That(output.ElementAt(i).GetMod().X, Is.EqualTo(expectedNumber.GetMod().X));
+                i++;
+            }
+        }
+
         /// <summary>
         /// This function is used to generate appropriate weights for net
         /// </summary>
@@ -286,16 +490,21 @@ namespace NeuroNet.Model.Tests.Net
 
             switch (_layer)
             {
+                    //hidden1
                 case 0:
+                    //first weight in first neuron
                     result = new RealNumber(1.0);
                     break;
                 case 1:
+                    //second weight in first neuron
                     result = new RealNumber(-1.5);
                     break;
                 case 2:
+                    //first weight in second neuron
                     result = new RealNumber(2.0);
                     break;
                 case 3:
+                    //second weight in second neuron
                     result = new RealNumber(1.0);
                     break;
 
@@ -313,17 +522,21 @@ namespace NeuroNet.Model.Tests.Net
                     result = new RealNumber(-1.0);
                     break;
 
-
+                    //output layer
                 case 8:
+                    //first weight in first neuron
                     result = new RealNumber(2.0);
                     break;
                 case 9:
+                    //first weight in second neuron
                     result = new RealNumber(4.0);
                     break;
                 case 10:
+                    //second weight in first neuron
                     result = new RealNumber(2.0);
                     break;
                 case 11:
+                    //second weight in second neuron
                     result = new RealNumber(2.0);
                     break;
 
