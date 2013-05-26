abc2278697381ed6aa528a783df31b9e60fb9aad
Diover <istatishere@gmail.com>
2013-05-26 11:42:32 -0700

    ConsoleApp. Added printing of net output on learned patterns after learning cycle.

--- NeuroNet.ConsoleApp/Program.cs
+++ NeuroNet.ConsoleApp/Program.cs
@@ -66,6 +66,7 @@ namespace NeuroNet.ConsoleApp
             //var net = new SimpleFuzzyNet(inputsCount, new[] {hiddenNeuronsCount}, () => DiscreteFuzzyNumber.GenerateLittleNumber(levelsCount: 11), levelsCount: 11);
             var net = new SimpleFuzzyNet(inputsCount, new[] { hiddenNeuronsCount }, RealNumber.GenerateLittleNumber, outputNeuronsCount: outputNeuronsCount);
             /*
+            //weights for 2-2-1 net for XOR function
             var weights = new Vector(new IFuzzyNumber[]
                 {
                     //output layer
@@ -114,9 +115,19 @@ namespace NeuroNet.ConsoleApp
 
             bp.LearnNet(net);
 
+            Console.WriteLine("Learning finished. Press any key...");
+            Console.ReadKey();
+            
             BinaryFileSerializer.SaveNetState(
                 "../../../Misc/LearnedNet " + Path.GetFileNameWithoutExtension(filename) +
                 " " + inputsCount + "-" + hiddenNeuronsCount + "-" + outputNeuronsCount + ".net", net);
+
+            var output = patterns.Select(pattern => net.Propagate(pattern.Input)).ToList();
+
+            for (int i = 0; i < patterns.Count; i++)
+            {
+                Console.WriteLine("Test: {0}, Real: {1}", string.Join(";", patterns.ElementAt(i).Output), string.Join(";", output.ElementAt(i)));
+            }
 
             Console.WriteLine("Finished. Press any key...");
             Console.ReadKey();
